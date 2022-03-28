43da7981cc56dd1d8fb6210ac9e49ec8bc919db5
Evans Kipngetich <kiprotich.evans.kipngetich@gmail.com>
2022-03-28 09:23:52 +0300

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,44 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😡",
+    name: "Pouting Face",
+    meaning: "A pouting face. A face that is angry and is about to throw up.",
+  },
+  {
+    id: 5,
+    emoji: "🥶",
+    name: "Freezing face",
+    meaning:
+      "It is so cold, you are about to freeze to death. The smile is almost frozen. Already totally blue in the face from the cold temperature, tortured facial expression. Only a cup of tea, coffee, or the hot tub can help now",
+  },
+  {
+    id: 6,
+    emoji: "🤔",
+    name: "Thinking Face",
+    meaning:"The thinker's pose: Thoughtful face with the characteristic hand on the chin. Puts a statement, a person's intelligence, or an idea into question. Is mulling over something or thinking about a brilliant idea."
+  },
+];
+
+export default emojipedia;
